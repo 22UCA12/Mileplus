ec45471bced4b44f2eeb1b5dcf7841c33ee88eec
Harini git <harinimoorthii2005@gmail.com>
2026-09-21 18:45:49 +0530

    mileplus

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.mileplus.mileplus"
+
+    compileSdk = flutter.compileSdkVersion
+
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    defaultConfig {
+        applicationId = "com.mileplus.mileplus"
+
+        minSdk = flutter.minSdkVersion
+
+        targetSdk = flutter.targetSdkVersion
+
+        versionCode = flutter.versionCode
+
+        versionName = flutter.versionName
+    }
+
+    buildTypes {
+        release {
+            signingConfig =
+                signingConfigs.getByName("debug")
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17
+        )
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    coreLibraryDesugaring(
+        "com.android.tools:desugar_jdk_libs:2.1.5"
+    )
+}
